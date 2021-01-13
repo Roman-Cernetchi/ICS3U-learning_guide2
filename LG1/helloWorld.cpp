d97e393239acd68fc6d0e219864406fa164d6087
Ubuntu <ubuntu@ip-172-31-60-141.ec2.internal>
2021-01-13 22:40:39 +0000

    hello world

---
+++ LG1/helloWorld.cpp
@@ -0,0 +1,16 @@
+// Copyright (c) 2019 St. Mother Teresa HS All rights reserved.
+//
+// Created by: Roman Cernetchi
+// Created on: January 2021
+// This program is the "Hello, World!" program for the GameBoy
+
+#include <stdio.h>
+#include <gb/gb.h>
+
+
+int main()
+{
+	printf("Hello, Mr. Coxall");
+	
+	return 0;
+}
